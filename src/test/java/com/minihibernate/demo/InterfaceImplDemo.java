e45bf0e34096d4b0f9bb1b21be743ee9708a5acf
lamtanloc512 <lamtanloc512@gmail.com>
2026-01-29 23:01:48 +0700

    clean up

--- src/test/java/com/minihibernate/demo/InterfaceImplDemo.java
+++ src/test/java/com/minihibernate/demo/InterfaceImplDemo.java
@@ -5,8 +5,10 @@ import java.sql.Connection;
 import com.minihibernate.api.MiniEntityManager;
 import com.minihibernate.api.MiniEntityManagerFactory;
 import com.minihibernate.api.MiniTransaction;
+import com.minihibernate.api.MiniTypedQuery;
 import com.minihibernate.example.User;
 import com.minihibernate.internal.MiniEntityManagerFactoryImpl;
+import com.minihibernate.internal.MiniEntityManagerImpl;
 
 import io.vavr.control.Try;
 
@@ -36,8 +38,6 @@ public class InterfaceImplDemo {
     System.out.println("  Actual:    " + factory.getClass().getName());
     System.out.println();
 
-    MiniEntityManager test = null;
-
     Try.withResources(() -> factory.createEntityManager())
         .of(em -> {
           MiniTransaction tx = em.getTransaction();
@@ -58,66 +58,64 @@ public class InterfaceImplDemo {
           System.out.println("User: " + u);
         }).andFinallyTry(() -> factory.close());
 
-    // try (factory) {
-    // // Create table first
-    // createTable(factory);
+    try (factory) {
+      // Create table first
+      createTable(factory);
 
-    // // 2. Create EntityManager - returns INTERFACE
-    // MiniEntityManager em = factory.createEntityManager();
+      // 2. Create EntityManager - returns INTERFACE
+      MiniEntityManager em = factory.createEntityManager();
 
-    // System.out.println("EntityManager type (what you see):");
-    // System.out.println(" Interface: MiniEntityManager");
-    // System.out.println(" Actual: " + em.getClass().getName());
-    // System.out.println();
+      System.out.println("EntityManager type (what you see):");
+      System.out.println(" Interface: MiniEntityManager");
+      System.out.println(" Actual: " + em.getClass().getName());
+      System.out.println();
 
-    // // 3. Get transaction - returns INTERFACE
-    // MiniTransaction tx = em.getTransaction();
+      // 3. Get transaction - returns INTERFACE
+      MiniTransaction tx = em.getTransaction();
 
-    // System.out.println("Transaction type (what you see):");
-    // System.out.println(" Interface: MiniTransaction");
-    // System.out.println(" Actual: " + tx.getClass().getName());
-    // System.out.println();
+      System.out.println("Transaction type (what you see):");
+      System.out.println(" Interface: MiniTransaction");
+      System.out.println(" Actual: " + tx.getClass().getName());
+      System.out.println();
 
-    // // 4. Demonstrate unwrap() - how to get actual implementation
-    // System.out.println("Using unwrap() to get actual implementation:");
-    // MiniEntityManagerImpl actualImpl = em.unwrap(MiniEntityManagerImpl.class);
-    // System.out.println(" Unwrapped: " + actualImpl.getClass().getName());
+      // 4. Demonstrate unwrap() - how to get actual implementation
+      System.out.println("Using unwrap() to get actual implementation:");
+      MiniEntityManagerImpl actualImpl = em.unwrap(MiniEntityManagerImpl.class);
+      System.out.println(" Unwrapped: " + actualImpl.getClass().getName());
 
-    // Connection conn = em.unwrap(Connection.class);
-    // System.out.println(" Connection: " + conn.getClass().getName());
-    // System.out.println();
+      Connection conn = em.unwrap(Connection.class);
+      System.out.println(" Connection: " + conn.getClass().getName());
+      System.out.println();
 
-    // // 5. Normal usage - only see interface methods
-    // tx.begin();
+      // 5. Normal usage - only see interface methods
+      tx.begin();
 
-    // User user = new User();
-    // user.setName("JPA Style User");
-    // user.setEmail("jpa@example.com");
+      User user = new User();
+      user.setName("JPA Style User");
+      user.setEmail("jpa@example.com");
 
-    // // When you debug persist(), you only see MiniEntityManager interface
-    // // But actual code runs in MiniEntityManagerImpl.persist()
-    // em.persist(user);
+      // When you debug persist(), you only see MiniEntityManager interface
+      // But actual code runs in MiniEntityManagerImpl.persist()
+      em.persist(user);
 
-    // tx.commit();
-    // System.out.println("Persisted user with ID: " + user.getId());
+      tx.commit();
+      System.out.println("Persisted user with ID: " + user.getId());
 
-    // // 6. Query - returns INTERFACE
-    // MiniTypedQuery<User> query = em.createQuery(User.class);
-    // System.out.println("\nQuery type (what you see):");
-    // System.out.println(" Interface: MiniTypedQuery");
-    // System.out.println(" Actual: " + query.getClass().getName());
+      // 6. Query - returns INTERFACE
+      MiniTypedQuery<User> query = em.createQuery(User.class);
+      System.out.println("\nQuery type (what you see):");
+      System.out.println(" Interface: MiniTypedQuery");
+      System.out.println(" Actual: " + query.getClass().getName());
 
-    // em.close();
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // }
+      em.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-    // System.out.println("\n=== Summary ===");
-    // System.out.println("You code against: com.minihibernate.api.* (interfaces)");
-    // System.out.println("Code executes in: com.minihibernate.internal.*
-    // (implementations)");
-    // System.out.println("\nThis is why you can't trace into implementation
-    // easily!");
+    System.out.println("\n=== Summary ===");
+    System.out.println("You code against: com.minihibernate.api.* (interfaces)");
+    System.out.println("Code executes in: com.minihibernate.internal.*(implementations)");
+    System.out.println("\nThis is why you can't trace into implementationeasily!");
   }
 
   private static void createTable(MiniEntityManagerFactory factory) {
